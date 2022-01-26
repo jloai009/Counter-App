5390bd2ae4365fd0a787d3606d1859ed42a95be1
Jose <jloai009@fiu.edu>
2022-01-26 17:59:17 -0500

    Reuse custom hook

--- src/App.js
+++ src/App.js
@@ -26,18 +26,34 @@ const useCounter = () => {
 const App = (props) => {
   const counter = useCounter()
 
+  const left = useCounter()
+  const right = useCounter()
+
   return (
     <div>
-      <div>{counter.value}</div>
-      <button onClick={counter.increase}>
-        plus
-      </button>
-      <button onClick={counter.decrease}>
-        minus
-      </button>
-      <button onClick={counter.zero}>
-        zero
-      </button>
+      <div>
+        <div>{counter.value}</div>
+        <button onClick={counter.increase}>
+          plus
+        </button>
+        <button onClick={counter.decrease}>
+          minus
+        </button>
+        <button onClick={counter.zero}>
+          zero
+        </button>
+      </div>
+      <br></br>
+      <div>
+        {left.value}
+        <button onClick={left.increase}>
+          left
+        </button>
+        <button onClick={right.increase}>
+          right
+        </button>
+        {right.value}
+      </div>
     </div>
   )
 }
